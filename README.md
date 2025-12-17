02ce16f5133c1c172bc174d6867d815b88fc38b8
Ashley Sirianni <ashleysiri1995@gmail.com>
2025-12-17 17:46:03 -0500

    my commmit on 1217

---
+++ README.md
@@ -0,0 +1,44 @@
+👋 Hi, I’m Ashley Sirianni
+Operations Administrator | Software Engineer in Training | Process Optimizer
+
+💡 About Me
+
+I’m a detail-oriented Operations Administrator with over eight years of experience managing vendor platforms, CRM systems, and high-volume operational workflows within the financial services industry. I excel at identifying inefficiencies, implementing structured solutions, and building clear, scalable processes through both operational systems and code.
+
+Currently pursuing my Software Engineering Degree through TripleTen, I’m developing strong technical expertise in:
+
+🖥️ Front-End Development:
+HTML5, CSS3, Flexbox, Grid, BEM, Media Queries, Transitions, JavaScript, JSX
+
+⚙️ Core Tools & Libraries:
+Git, GitHub, Webpack, NPM, React, React Components, Form Validation, OOP, DOM Manipulation, Debugging, Figma
+
+🚀 What I Do Best:
+
+Develop clean, responsive, and semantic front-end code.
+
+Translate complex operations into automated workflows.
+
+Create standard operating procedures (SOPs) that scale across teams.
+
+Collaborate cross-functionally to improve process efficiency and data accuracy.
+
+Bridge the gap between technical systems and business strategy.
+
+## Project Pitch Videos
+
+Check out these videos, where I describe my project
+and some challenges I faced while building it:
+
+https://docs.google.com/document/d/1mYWJhrEYnuDs7c7UJzqsRzP4M7-nTt_nk7jp1xS3mVc/edit?usp=drive_link
+
+https://ashleysiri.github.io/se_project_spots
+
+https://drive.google.com/file/d/1tUFfYOLeeWyIj8jGM62shNoFaxA3s16V/view?usp=drive_link
+
+- Intro
+- Figma
+- Images
+  **Intro**
+
+This project is made so all the elements are displayed correctly on popular screen sizes.
---
+++ scripts/validation.js
@@ -0,0 +1,89 @@
+const settings = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__submit-btn",
+  inactiveButtonClass: "modal__submit-btn_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+const showInputError = (formEl, inputEl, errorMsg, config) => {
+  const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
+  if (!errorMsgEl) return;
+
+  errorMsgEl.textContent = errorMsg;
+  inputEl.classList.add(config.inputErrorClass);
+  errorMsgEl.classList.add(config.errorClass);
+};
+
+const hideInputError = (formEl, inputEl, config) => {
+  const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
+  if (!errorMsgEl) return;
+
+  errorMsgEl.textContent = "";
+  inputEl.classList.remove(config.inputErrorClass);
+  errorMsgEl.classList.remove(config.errorClass);
+};
+
+const checkInputValidity = (formEl, inputEl, config) => {
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, inputEl.validationMessage, config);
+  } else {
+    hideInputError(formEl, inputEl, config);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((input) => !input.validity.valid);
+};
+
+const disableButton = (buttonEl, config) => {
+  buttonEl.disabled = true;
+  buttonEl.classList.add(config.inactiveButtonClass);
+};
+
+const toggleButtonState = (inputList, buttonEl, config) => {
+  if (hasInvalidInput(inputList)) {
+    disableButton(buttonEl, config);
+  } else {
+    buttonEl.disabled = false;
+
+    buttonEl.classList.remove(config.inactiveButtonClass);
+  }
+};
+
+const resetValidation = (formEl, inputList, config) => {
+  inputList.forEach((input) => {
+    hideInputError(formEl, input, config);
+  });
+
+  const buttonElement = formEl.querySelector(config.submitButtonSelector);
+  if (buttonElement) {
+    toggleButtonState(inputList, buttonElement, config);
+  }
+};
+
+const setEventListeners = (formElement, config) => {
+  const inputList = Array.from(
+    formElement.querySelectorAll(config.inputSelector)
+  );
+  const buttonElement = formElement.querySelector(config.submitButtonSelector);
+
+  toggleButtonState(inputList, buttonElement, config);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      checkInputValidity(formElement, inputElement, config);
+      toggleButtonState(inputList, buttonElement, config);
+    });
+  });
+};
+
+const enableValidation = (config) => {
+  const formList = document.querySelectorAll(config.formSelector);
+  formList.forEach((formEl) => {
+    setEventListeners(formEl, config);
+  });
+};
+
+enableValidation(settings);
